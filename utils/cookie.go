887269f072791b91573e1babf9237a9f3ee605d8
anyingiit <49945850+anyingiit@users.noreply.github.com>
2022-10-14 01:17:32 +0000

    fix: if the parameter cookieString is empty string, then will panic `runtime error: index out of range [1] with length 1`

--- utils/cookie.go
+++ utils/cookie.go
@@ -4,10 +4,12 @@ import "strings"
 
 func ParseCookieValue(cookieStr string, key string) string {
 	// `target=456; a=123` -> `456; a=123`
-	value := strings.Split(cookieStr, key+"=")[1]
-
-	// `456; a=123` -> `456`
-	value = strings.Split(value, ";")[0]
-
-	return value
+	if s1 := strings.Split(cookieStr, key+"="); len(s1) >= 2 {
+		// `456` do nothing -> `456`
+		// `456; a=123` -> `456`
+		if s2 := strings.Split(s1[1], ";"); len(s2) >= 1 {
+			return s2[0]
+		}
+	}
+	return ""
 }
