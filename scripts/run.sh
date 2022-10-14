903199ccf48cc585396cf208eea11d6d8757529a
anyingiit <49945850+anyingiit@users.noreply.github.com>
2022-10-14 14:15:46 +0000

    update docker build & run scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,6 @@ docker run -t --name unify_auth_center \
   -p 8066:8066 \
   --network http-proxy-nginx-bridge \
   -e TZ="Asia/Shanghai" \
-  unify_auth_center:V0.1 \
+  anyingiit/unify_auth_center:V0.1 \
 \
 & docker logs -f unify_auth_center
