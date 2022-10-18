83b5359cfb90fb527e289126b28406188c9e0e4a
anyingiit <49945850+anyingiit@users.noreply.github.com>
2022-10-18 07:23:44 +0000

    update docker image version

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,5 +4,5 @@ docker run -d \
   -p 8066:8066 \
   --network http-proxy-nginx-bridge \
   -e TZ="Asia/Shanghai" \
-  anyingiit/unify_auth_center:V0.5 \
+  anyingiit/unify_auth_center:V0.6 \
 && docker logs -f unify_auth_center
